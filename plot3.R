ea035e8f601e8d3290388614066e334213de3180
ddurden <daviddurden27@gmail.com>
2015-05-10 12:08:18 -0600

    updating plots.

--- plot3.R
+++ plot3.R
@@ -15,10 +15,10 @@ timestamp <- strptime(paste(power_days$Date,power_days$Time), "%Y-%m-%d %H:%M:%S
 png(filename = "plot3.png",width = 480, height = 480)
 
 #Plotting a line graph of Global Active Power
-plot(x = timestamp, y = power_days$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub meeting")
-lines(x = timestamp, y = power_days$Sub_metering_2, type = "l", col = "blue")
-lines(x = timestamp, y = power_days$Sub_metering_3, type = "l", col = "red")
-legend(x ="topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1,1), lwd = c(2.5,2.5,2.5), col = c("black", "blue", "red"))
+plot(x = timestamp, y = power_days$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+lines(x = timestamp, y = power_days$Sub_metering_2, type = "l", col = "red")
+lines(x = timestamp, y = power_days$Sub_metering_3, type = "l", col = "blue")
+legend(x ="topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1,1), lwd = c(2.5,2.5,2.5), col = c("black", "red", "blue"))
 
 
 #Turning off the graphics device
--- plot4.R
+++ plot4.R
@@ -17,16 +17,16 @@ png(filename = "plot4.png",width = 480, height = 480)
 #Settings for layout of plots 
 par(mfrow = (c(2,2)))
 
-plot(x = timestamp, y = power_days$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+plot(x = timestamp, y = power_days$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
 
 plot(x = timestamp, y = power_days$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
 
 
 #Plotting a line graph of Global Active Power
-plot(x = timestamp, y = power_days$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub meeting")
-lines(x = timestamp, y = power_days$Sub_metering_2, type = "l", col = "blue")
-lines(x = timestamp, y = power_days$Sub_metering_3, type = "l", col = "red")
-legend(x ="topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1,1), lwd = c(2.5,2.5,2.5), col = c("black", "blue", "red"), bty = "n")
+plot(x = timestamp, y = power_days$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+lines(x = timestamp, y = power_days$Sub_metering_2, type = "l", col = "red")
+lines(x = timestamp, y = power_days$Sub_metering_3, type = "l", col = "blue")
+legend(x ="topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1,1), lwd = c(2.5,2.5,2.5), col = c("black", "red", "blue"), bty = "n")
 
 plot(x = timestamp, y = power_days$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
 
